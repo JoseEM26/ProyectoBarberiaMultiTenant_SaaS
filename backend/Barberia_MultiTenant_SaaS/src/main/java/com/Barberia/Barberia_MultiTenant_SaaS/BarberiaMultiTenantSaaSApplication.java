b2a1f82252a55352dceefb990e7e1b9a506010f6
JoseEM26 <joseangelespinozamorales@gmail.com>
2025-11-06 15:25:19 -0500

    start backend

---
+++ backend/Barberia_MultiTenant_SaaS/src/main/java/com/Barberia/Barberia_MultiTenant_SaaS/BarberiaMultiTenantSaaSApplication.java
@@ -0,0 +1,13 @@
+package com.Barberia.Barberia_MultiTenant_SaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberiaMultiTenantSaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberiaMultiTenantSaaSApplication.class, args);
+	}
+
+}
---
+++ backend/Barberia_MultiTenant_SaaS/src/test/java/com/Barberia/Barberia_MultiTenant_SaaS/BarberiaMultiTenantSaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barberia.Barberia_MultiTenant_SaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberiaMultiTenantSaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
